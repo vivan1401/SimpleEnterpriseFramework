2eac96e5ca9264248abbe7b6d915871886712f4b
Vi Van <vivanpro1401@gmail.com>
2019-01-06 02:50:54 +0700

    demo done

---
+++ Demo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Framework;
+using DoAnFramwork;
+using MemberShip;
+using System.Drawing;
+
+namespace Demo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false); try
+            {
+                DatabaseConnection databaseConnection = new DatabaseMSSQLConnection("SimpleDatabase", @"VIVAN\SQLEXPRESS");
+                ReadRole readRole = new DefaultReadRoleSeparate();
+                FormAdd myFormAdd = new FormAdd(FormType.Add, "My Form Add", new Size(570, 345), databaseConnection);
+                FormUpdate myFormUpdate = new FormUpdate(FormType.Update, "My Form Update", new Size(570, 345), databaseConnection);
+                myFormAdd.BackColor = Color.Aqua;
+                myFormUpdate.BackColor = Color.Green;
+
+                FormMain myFormMain = new FormMain(FormType.Main, "My Form Main", new Size(570, 345), databaseConnection);
+                myFormAdd.SetupForm();
+                myFormUpdate.SetupForm();
+                myFormMain.SetFormAdd(myFormAdd);
+                myFormMain.SetFormUpdate(myFormUpdate);
+                FormLogin formLogin = new FormLogin(databaseConnection, readRole, myFormMain);
+                Application.Run(formLogin);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
