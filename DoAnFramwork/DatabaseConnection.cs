9d7d41ba3d77f76f4be9f9d76533a8c7dc0af255
Xuân Tin <xuantinfx@gmail.com>
2019-01-05 15:57:30 +0700

    database connection

---
+++ DoAnFramwork/DatabaseConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework{
+    public interface DatabaseConnection{
+        SqlDataReader readData(String tableName);
+        int insert(String tableName, Object[] values);
+        int update(String tableName,Object[] values);
+        int delete(String tableName,Object obj);
+        Dictionary<String,Type> getField();
+    }
+}
